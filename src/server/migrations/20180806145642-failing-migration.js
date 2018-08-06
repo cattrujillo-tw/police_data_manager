efbe8657794f32bd11d76eac58822600d07c7585
Phoebe Wooldridge <pwooldri@thoughtworks.com>
2018-08-06 10:08:44 -0500

    Phoebe #000 push with failing migration - no fix yet

---
+++ src/server/migrations/20180806145642-failing-migration.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    throw new Error("Migration up failed");
+  },
+
+  down: (queryInterface, Sequelize) => {
+    throw new Error("Migration down failed");
+  }
+};
