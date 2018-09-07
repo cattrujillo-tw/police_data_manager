9c2230d2bbbccffb0676a146db88486efcd16206
Phoebe Wooldridge <pwooldri@thoughtworks.com>
2018-09-07 16:40:44 -0500

    Phoebe/Sarah #445 use ubuntu and add image and styling in header -- saves to file instead of buffer

--- src/server/generateLetter.js
+++ src/server/generateLetter.js
@@ -1,38 +1,20 @@
 const pdf = require("html-pdf");
 
 generateLetter = (request, response) => {
-  console.log("IN HERE");
-  //const html = document.getElementsByTagName("main")[0].innerHTML;
-  console.log("Request body:", request.body);
-  let pdfContent = null;
   pdf
     .create(request.body.html, {
       phantomPath: "/usr/bin/phantomjs",
       timeout: 100000,
       format: "Letter"
     })
-    .toBuffer(function(err, buffer) {
+    .toFile("testpdf.pdf", function(err, res) {
       if (err) {
         console.log("Error:", err);
         throw err;
       }
-      console.log("This is a buffer:", Buffer.isBuffer(buffer));
-      pdfContent = buffer;
+      console.log("This is the filename:", res);
     });
-
-  response.send(pdfContent);
-  //return pdfContent;
-
-  //   const htmlDoc = document.getElementsByTagName("main")[0];
-  //   const converted = await generateDocx(htmlDoc.innerHTML);
-  //
-  //   console.log("Word doc is :", converted);
-  //   return {
-  //     mime:
-  //       "application/pdf",
-  //     filename: "caseLetter.docx",
-  //     contents: converted
-  //   };
+  response.send("");
 };
 
 module.exports = generateLetter;
