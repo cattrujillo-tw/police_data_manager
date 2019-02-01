e53fc8d64c26a58f83cb20480ba696a8aeb9d311
Susie Riley <sriley@thoughtworks.com>
2019-02-01 11:25:37 -0600

    Susie #424 Fail build when migrations or migration tasks fail

--- src/server/migrations/20190201164724-successful-test-migration.js
+++ src/server/migrations/20190201164724-successful-test-migration.js
@@ -3,13 +3,13 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     throw Error(
-      "this error should not be thrown because the migration up will already have run"
+      "this error should not be thrown because the migration up will already have run successfully"
     );
   },
 
   down: (queryInterface, Sequelize) => {
     throw Error(
-      "this error should not be thrown because the migration down will already have run"
+      "this error should not be thrown because the migration down will already have run successfully"
     );
   }
 };
