9741f5c22b5f61539f4b4b05538edd64ddc43e88
Susie Riley <sriley@thoughtworks.com>
2019-02-01 10:48:11 -0600

    Susie #424 Push with failing migration - should fail build

---
+++ src/server/migrations/20190201164724-failing-migration.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    throw Error("migrate up failed");
+  },
+
+  down: (queryInterface, Sequelize) => {
+    throw Error("migrate down failed");
+  }
+};
