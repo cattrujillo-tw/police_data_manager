e74976570ee09c2e440194d0aacac9f793a5e3c7
Susie Riley <sriley@thoughtworks.com>
2019-02-01 10:49:23 -0600

    Susie #424 Push with failing migration and migration task - should fail build

---
+++ src/server/tasks/migrations/003_failing_migration_task.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    throw Error("migrate task up failed");
+  },
+
+  down: (queryInterface, Sequelize) => {
+    throw Error("migrate task down failed");
+  }
+};
