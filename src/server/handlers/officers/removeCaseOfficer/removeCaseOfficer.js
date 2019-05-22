be360725a2756609b5774d3c4bd856769cd03d60
Susie Riley <sriley@thoughtworks.com>
2019-05-22 09:30:20 -0700

    Revert "Susie/Jacob [#165801763] Implement new auditing for removeCaseOfficer"
    This reverts commit d45df6f9358233142652d90b8313776374f716fa.

--- src/server/handlers/officers/removeCaseOfficer/removeCaseOfficer.js
+++ src/server/handlers/officers/removeCaseOfficer/removeCaseOfficer.js
@@ -2,8 +2,6 @@ import { BAD_REQUEST_ERRORS } from "../../../../sharedUtilities/errorMessageCons
 import { getCaseWithAllAssociations } from "../../getCaseHelpers";
 import legacyAuditDataAccess from "../../legacyAuditDataAccess";
 import { AUDIT_ACTION } from "../../../../sharedUtilities/constants";
-import checkFeatureToggleEnabled from "../../../checkFeatureToggleEnabled";
-import auditDataAccess from "../../auditDataAccess";
 
 const models = require("../../../models");
 const asyncMiddleware = require("../../asyncMiddleware");
@@ -11,11 +9,6 @@ const Boom = require("boom");
 const { AUDIT_SUBJECT } = require("../../../../sharedUtilities/constants");
 
 const removeCaseOfficer = asyncMiddleware(async (request, response, next) => {
-  const newAuditFeatureToggle = checkFeatureToggleEnabled(
-    request,
-    "newAuditFeature"
-  );
-
   const officerToRemove = await models.case_officer.findByPk(
     request.params.caseOfficerId
   );
@@ -38,24 +31,14 @@ const removeCaseOfficer = asyncMiddleware(async (request, response, next) => {
       auditDetails
     );
 
-    if (newAuditFeatureToggle) {
-      await auditDataAccess(
-        request.nickname,
-        request.params.caseId,
-        AUDIT_SUBJECT.CASE_DETAILS,
-        auditDetails,
-        transaction
-      );
-    } else {
-      await legacyAuditDataAccess(
-        request.nickname,
-        request.params.caseId,
-        AUDIT_SUBJECT.CASE_DETAILS,
-        transaction,
-        AUDIT_ACTION.DATA_ACCESSED,
-        auditDetails
-      );
-    }
+    await legacyAuditDataAccess(
+      request.nickname,
+      request.params.caseId,
+      AUDIT_SUBJECT.CASE_DETAILS,
+      transaction,
+      AUDIT_ACTION.DATA_ACCESSED,
+      auditDetails
+    );
 
     return caseDetails;
   });
