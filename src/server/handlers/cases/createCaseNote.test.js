c70723a92d4939d30c0c36117f0dce92383e2e36
Susie Riley <sriley@thoughtworks.com>
2019-05-22 09:05:27 -0700

    Revert "Susie/Jacob [#165801763] Implement new auditing for createCaseNote"
    This reverts commit f5ccf082f2939c8274155eb41a31ed13e4fa332b.

--- src/server/handlers/cases/createCaseNote.test.js
+++ src/server/handlers/cases/createCaseNote.test.js
@@ -10,16 +10,12 @@ import {
 import createCaseNote from "./createCaseNote";
 import * as httpMocks from "node-mocks-http";
 import moment from "moment";
-import auditDataAccess from "../auditDataAccess";
-import mockFflipObject from "../../testHelpers/mockFflipObject";
 
 //mocked implementation in "/handlers/__mocks__/getQueryAuditAccessDetails"
 jest.mock("../getQueryAuditAccessDetails");
 
-jest.mock("../auditDataAccess");
-
 describe("createCaseNote", function() {
-  let createdCase, request, response, next;
+  let createdCase;
 
   afterEach(async () => {
     await cleanupDatabase();
@@ -39,7 +35,10 @@ describe("createCaseNote", function() {
     createdCase = await models.cases.create(caseToCreate, {
       auditUser: "someone"
     });
-    request = httpMocks.createRequest({
+  });
+
+  test("should audit case note accessed", async () => {
+    const request = httpMocks.createRequest({
       method: "POST",
       headers: {
         authorization: "Bearer SOME_MOCK_TOKEN"
@@ -50,91 +49,60 @@ describe("createCaseNote", function() {
       },
       nickname: "TEST_USER_NICKNAME"
     });
-    response = httpMocks.createResponse();
-    next = jest.fn();
+
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+    await createCaseNote(request, response, next);
+
+    const actionAudit = await models.action_audit.findAll({
+      where: { caseId: createdCase.id }
+    });
+
+    expect(actionAudit).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          user: "TEST_USER_NICKNAME",
+          auditType: AUDIT_TYPE.DATA_ACCESS,
+          action: AUDIT_ACTION.DATA_ACCESSED,
+          subject: AUDIT_SUBJECT.CASE_NOTES,
+          caseId: createdCase.id
+        })
+      ])
+    );
   });
 
-  describe("newAuditFeature is disabled", () => {
-    test("should audit case note accessed", async () => {
-      request.fflip = mockFflipObject({ newAuditFeature: false });
-      await createCaseNote(request, response, next);
-
-      const actionAudit = await models.action_audit.findAll({
-        where: { caseId: createdCase.id }
-      });
-
-      expect(actionAudit).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            user: "TEST_USER_NICKNAME",
-            auditType: AUDIT_TYPE.DATA_ACCESS,
-            action: AUDIT_ACTION.DATA_ACCESSED,
-            subject: AUDIT_SUBJECT.CASE_NOTES,
-            caseId: createdCase.id
-          })
-        ])
-      );
+  test("should audit case details accessed", async () => {
+    const request = httpMocks.createRequest({
+      method: "POST",
+      headers: {
+        authorization: "Bearer SOME_MOCK_TOKEN"
+      },
+      body: { action: "some action", actionTakenAt: moment() },
+      params: {
+        caseId: createdCase.id
+      },
+      nickname: "TEST_USER_NICKNAME"
     });
 
-    test("should audit case details accessed", async () => {
-      request.fflip = mockFflipObject({ newAuditFeature: false });
-      await createCaseNote(request, response, next);
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+    await createCaseNote(request, response, next);
 
-      const actionAudit = await models.action_audit.findAll({
-        where: { caseId: createdCase.id }
-      });
-
-      expect(actionAudit).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            user: "TEST_USER_NICKNAME",
-            auditType: AUDIT_TYPE.DATA_ACCESS,
-            action: AUDIT_ACTION.DATA_ACCESSED,
-            subject: AUDIT_SUBJECT.CASE_DETAILS,
-            caseId: createdCase.id,
-            auditDetails: { ["Mock Association"]: ["Mock Details"] }
-          })
-        ])
-      );
+    const actionAudit = await models.action_audit.findAll({
+      where: { caseId: createdCase.id }
     });
-  });
 
-  describe("newAuditFeature is enabled", () => {
-    test("should audit case note accessed", async () => {
-      request.fflip = mockFflipObject({ newAuditFeature: true });
-
-      await createCaseNote(request, response, next);
-
-      expect(auditDataAccess).toHaveBeenCalledWith(
-        request.nickname,
-        createdCase.id,
-        AUDIT_SUBJECT.CASE_NOTES,
-        {
-          caseNote: {
-            attributes: ["mockDetails"],
-            model: models.case_note.name
-          }
-        },
-        expect.anything()
-      );
-    });
-    test("should audit case details accessed", async () => {
-      request.fflip = mockFflipObject({ newAuditFeature: true });
-
-      await createCaseNote(request, response, next);
-
-      expect(auditDataAccess).toHaveBeenCalledWith(
-        request.nickname,
-        createdCase.id,
-        AUDIT_SUBJECT.CASE_DETAILS,
-        {
-          mockAssociation: {
-            attributes: ["mockDetails"],
-            model: "mockModelName"
-          }
-        },
-        expect.anything()
-      );
-    });
+    expect(actionAudit).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          user: "TEST_USER_NICKNAME",
+          auditType: AUDIT_TYPE.DATA_ACCESS,
+          action: AUDIT_ACTION.DATA_ACCESSED,
+          subject: AUDIT_SUBJECT.CASE_DETAILS,
+          caseId: createdCase.id,
+          auditDetails: { ["Mock Association"]: ["Mock Details"] }
+        })
+      ])
+    );
   });
 });
