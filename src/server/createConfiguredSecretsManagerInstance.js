60ade2d0c8e0b5140ecb2a9c1091e180293da540
KatieMLarson <katie.larson@thoughtworks.com>
2019-09-25 10:48:06 -0500

    [Karan/Katie][#167180246] POC for integrating with AWS Secrets Manager

---
+++ src/server/createConfiguredSecretsManagerInstance.js
@@ -0,0 +1,13 @@
+const AWS = require("aws-sdk");
+const path = require("path");
+
+const createConfiguredSecretsManagerInstance = () => {
+  const secretsManager  = new AWS.SecretsManager({
+    region: "us-east-2"
+  });
+
+  secretsManager.config.loadFromPath(path.join(__dirname, "./awsConfig.json"));
+
+  return secretsManager;
+};
+module.exports = createConfiguredSecretsManagerInstance;
--- src/server/disciplinaryProceedings/handlers/users/getUsers.js
+++ src/server/disciplinaryProceedings/handlers/users/getUsers.js
@@ -4,7 +4,9 @@ import { INTERNAL_ERRORS } from "../../../../sharedUtilities/errorMessageConstan
 import auditDataAccess from "../../../handlers/audits/auditDataAccess";
 import models from "../../../models";
 import { AUDIT_SUBJECT } from "../../../../sharedUtilities/constants";
+import createConfiguredSecretsManagerInstance from "../../../createConfiguredSecretsManagerInstance.js";
 
+//const secretsManager = require("../../../createConfiguredSecretsManagerInstance.js");
 const querystring = require("querystring");
 const config = require("../../../config/config")[process.env.NODE_ENV];
 const asyncMiddleware = require("../../../handlers/asyncMiddleware");
@@ -14,6 +16,70 @@ const Boom = require("boom");
 const getUsers = asyncMiddleware(async (request, response, next) => {
   let authResponse;
   let userData = [];
+  let secret;
+  let decodedAuthSecret;
+
+  if (
+    process.env.NODE_ENV === "development" ||
+    process.env.NODE_ENV === "test"
+  ) {
+    let secretsManager = createConfiguredSecretsManagerInstance();
+
+    const request = secretsManager.getSecretValue({
+      SecretId: "ci/auth0/backend"
+    });
+    const result = request.promise();
+    await result
+      .then(function(data) {
+        console.log("I'm inside!", data);
+
+        console.log("Made it past the errors!");
+        // Decrypts secret using the associated KMS CMK.
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if ("SecretString" in data) {
+          console.log(
+            "I made it! Here's my secret string: ",
+            data.SecretString
+          );
+          secret = JSON.parse(data.SecretString);
+        } else {
+          let buff = new Buffer(data.SecretBinary, "base64");
+          decodedAuthSecret = buff.toString("ascii");
+        }
+
+        // Your code goes here.
+        console.log("this is aws secret: ", secret);
+        if (secret) {
+          secret = secret.AUTH0_CLIENT_SECRET;
+          console.log("this is the Auth0 client secret: ", secret);
+        }
+      })
+      .catch(function(err) {
+        console.error("I failed :(", err);
+        if (err.code === "DecryptionFailureException")
+          // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+          // Deal with the exception here, and/or rethrow at your discretion.
+          throw err;
+        else if (err.code === "InternalServiceErrorException")
+          // An error occurred on the server side.
+          // Deal with the exception here, and/or rethrow at your discretion.
+          throw err;
+        else if (err.code === "InvalidParameterException")
+          // You provided an invalid value for a parameter.
+          // Deal with the exception here, and/or rethrow at your discretion.
+          throw err;
+        else if (err.code === "InvalidRequestException")
+          // You provided a parameter value that is not valid for the current state of the resource.
+          // Deal with the exception here, and/or rethrow at your discretion.
+          throw err;
+        else if (err.code === "ResourceNotFoundException")
+          // We can't find the resource that you asked for.
+          // Deal with the exception here, and/or rethrow at your discretion.
+          throw err;
+      });
+  } else {
+    secret = process.env.AUTH0_CLIENT_SECRET;
+  }
 
   await axios
     .post(
@@ -21,7 +87,7 @@ const getUsers = asyncMiddleware(async (request, response, next) => {
       {
         grant_type: "client_credentials",
         client_id: config.authentication.clientID,
-        client_secret: process.env.AUTH0_CLIENT_SECRET,
+        client_secret: secret,
         audience: `https://${config.authentication.domain}/api/v2/`
       },
       {
