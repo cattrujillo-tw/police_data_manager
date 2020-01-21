9f29805fbc49ab04d3181648711dea36387df6ca
Karan Krishnani <krkrishn@thoughtworks.com>
2020-01-21 16:08:08 -0500

    add subscription from frontend, need to add backend notifications next

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import { render } from "react-dom";
 import "./index.css";
 import RootContainer from "./client/RootContainer";
 import * as serviceWorker from "./serviceWorker";
+import { subscribeUser } from "./subscription";
 
 render(<RootContainer />, document.getElementById("root"));
 
 console.log("public url: ", process.env);
 
 serviceWorker.register();
+
+subscribeUser();
