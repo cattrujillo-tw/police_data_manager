9a79979b21bbe90396bb2e0c31fa14722d743448
Karan Krishnani <krkrishn@thoughtworks.com>
2020-08-20 14:56:33 -0400

    Ray/Karan [#174410386] Adds script to pull down instance files from S3

---
+++ scripts/get_instance_files.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+BUCKET_NAME=noipm-private-images
+DEST_DIRECTORY=./src/instance_files
+
+mkdir -p $DEST_DIRECTORY
+
+if [ -z $CI ]
+then
+    aws --profile noipm-personal s3 cp s3://$BUCKET_NAME $DEST_DIRECTORY --recursive
+else
+    AWS_ACCESS_KEY_ID=$CI_AWS_ACCESS_KEY_ID
+    AWS_SECRET_ACCESS_KEY=$CI_AWS_SECRET_ACCESS_KEY
+
+    aws s3 cp s3://$BUCKET_NAME $DEST_DIRECTORY --recursive
+fi
