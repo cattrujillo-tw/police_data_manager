250bf9b3253a458996ff6d36844830a077f94fdf
Andrew Rist <andrew.rist@thoughtworks.com>
2021-08-30 13:00:08 -0500

    different districts different colors

--- src/client/publicDataDashboard/MapVisualization.js
+++ src/client/publicDataDashboard/MapVisualization.js
@@ -34,22 +34,64 @@ const MapVisualization = props => (
         layers: [
           {
             sourcetype: "geojson",
-            source: districts,
+            source: district1,
             type: "fill",
             color: "rgba(0, 255, 0, 0.5)"
           },
           {
             sourcetype: "geojson",
-            source: headquarters,
-            type: "symbol",
-            layout: {
-              "text-field": ["get", "description"],
-              "text-variable-anchor": ["top", "bottom", "left", "right"],
-              "text-radial-offset": 0.5,
-              "text-justify": "auto",
-              "icon-image": "village"
-            }
+            source: district2,
+            type: "fill",
+            color: "rgba(255, 0, 0, 0.5)"
+          },
+          {
+            sourcetype: "geojson",
+            source: district3,
+            type: "fill",
+            color: "rgba(0, 0, 0, 0.5)"
+          },
+          {
+            sourcetype: "geojson",
+            source: district4,
+            type: "fill",
+            color: "rgba(255, 255, 255, 0.5)"
+          },
+          {
+            sourcetype: "geojson",
+            source: district5,
+            type: "fill",
+            color: "rgba(0, 0, 255, 0.5)"
+          },
+          {
+            sourcetype: "geojson",
+            source: district6,
+            type: "fill",
+            color: "rgba(255, 0, 255, 0.5)"
+          },
+          {
+            sourcetype: "geojson",
+            source: district7,
+            type: "fill",
+            color: "rgba(0, 255, 255, 0.5)"
+          },
+          {
+            sourcetype: "geojson",
+            source: district8,
+            type: "fill",
+            color: "rgba(255, 255, 0, 0.5)"
           }
+          // {
+          //     sourcetype: "geojson",
+          //     source: headquarters,
+          //     type: "symbol",
+          //     layout: {
+          //         "text-field": ["get", "description"],
+          //         "text-variable-anchor": ["top", "bottom", "left", "right"],
+          //         "text-radial-offset": 0.5,
+          //         "text-justify": "auto",
+          //         "icon-image": "village"
+          //     }
+          // }
         ]
       },
       margin: { r: 0, t: 0, b: 0, l: 0 }
