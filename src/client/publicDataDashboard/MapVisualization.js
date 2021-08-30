9efbf5837b26d2f6d6cd52e54efc47b5dbcc4bfc
Andrew Rist <andrew.rist@thoughtworks.com>
2021-08-30 10:18:11 -0500

    translucent districts

--- src/client/publicDataDashboard/MapVisualization.js
+++ src/client/publicDataDashboard/MapVisualization.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { PlotlyWrapper } from "../common/components/Visualization/PlotlyWrapper";
 import districts from "./assets/locationData/NOPD_Police_Districts.json";
+import headquarters from "./assets/locationData/districtHeadquarters.json";
+import district1 from "./assets/locationData/district1.json";
+import district2 from "./assets/locationData/district2.json";
+import district3 from "./assets/locationData/district3.json";
+import district4 from "./assets/locationData/district4.json";
+import district5 from "./assets/locationData/district5.json";
+import district6 from "./assets/locationData/district6.json";
+import district7 from "./assets/locationData/district7.json";
+import district8 from "./assets/locationData/district8.json";
 
 const NOLA_CENTER = { lat: 29.947, lon: -90.07 };
 const DEFAULT_ZOOM = 10;
@@ -27,7 +36,19 @@ const MapVisualization = props => (
             sourcetype: "geojson",
             source: districts,
             type: "fill",
-            color: "rgba(255, 100, 50, 0.5)"
+            color: "rgba(0, 255, 0, 0.5)"
+          },
+          {
+            sourcetype: "geojson",
+            source: headquarters,
+            type: "symbol",
+            layout: {
+              "text-field": ["get", "description"],
+              "text-variable-anchor": ["top", "bottom", "left", "right"],
+              "text-radial-offset": 0.5,
+              "text-justify": "auto",
+              "icon-image": "village"
+            }
           }
         ]
       },
